f58ecfde09b4dc4e0721ec03462c1f16d8fa9153
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-07-20 16:11:50 -0700

    HTML ellement and +=operator assignment
    I was testing a few things out .
    
    Added
    -Basic_HTML_2.html
    
    in folder JS
    -Basic_JavaScript_2.js

--- JS/Basic_JavaScript_1.js
+++ JS/Basic_JavaScript_1.js
@@ -2,7 +2,7 @@ var S = "\"This is a string\" Jon said, as he read his assignment aloud, with hi
 + " Or did he\?", 
 Y = "Does 3 + 3 = 6...Yes, yes it does.";   //Assigned multiple variables.
 A = "So... he did?",
-B = "Yes."                    
+B = "Yes.";                    
 var Y = Y.fontcolor("orange");              //Changing text color of var Y.
 window.alert(S);                            //Window pop-up.
 document.write(Y+' '+A+' '+B);              //Text on the page.
