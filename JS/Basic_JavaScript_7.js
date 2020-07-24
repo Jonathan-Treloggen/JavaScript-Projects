10dac910b08dfeba7264e07b6f170e70483be7f2
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-07-24 10:07:03 -0700

    HTML and JavaScript assinment 6, and 7.
    Added
    -Basic_HTML_6.html
    -Basic_HTML_7.html
    
    in folder JS
    -Basic_JavaScript_6.js
    -Basic_JavaScript_7.js

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -1,14 +1,75 @@
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
+// FIRST ASSIGNMENT
+function Error1_Function() {
+    var E = 15;
+    console.log(23 + E);
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML =
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
-    " manufactured in " + Erik.Vehicle_Year;
+function Error2_Function() {
+    console.log(E + 15);
 }
+
+function Local1_Function() {
+    var R = 20;
+    document.write(R + 12);  
+}
+function Local2_Function() {
+    document.write(R + 12);  
+}
+
+var I = 72;
+function Global1_Function() {
+    document.write(I + 5);
+}
+function Global2_Function() {
+    document.write(83 + I);
+}
+// END OF FIRST ASSIGNMENT
+
+
+// SECOND ASSIGNMENT
+function the_hour() {
+    if (new Date() .getHours() < 18) {
+        document.getElementById("or").innerHTML = "How are you today?";
+    }
+}
+
+function Path() {
+    var Xbox = 72;
+    var PS5 =60;
+    if (Xbox > PS5) {
+        document.getElementById("Correct").innerHTML = "Xbox is better the PS, believe it. Now you know the truth.";
+    }
+}
+// END OF SECOND ASSIGNMENT
+
+
+// THIRD ASSIGNMENT
+function Ben_PR() {
+    FAV = document.getElementById("FAV").value;
+    var Cal = "Calzone";
+    if (FAV == Cal) {
+        Choice = "Ben Cubison from Parks and rec,  and myself would agree with you.";
+    }
+    else {
+        Choice = "Wrong, Park and rec will give you the answer. Hint Ben.";
+    }
+    document.getElementById("answer").innerHTML = Choice;
+}
+// END OF THIRD ASSIGNMENT
+
+
+// FOURTH ASSIGNMENT
+function Time_of_day() {
+    var Time = new Date().getHours();
+    var Response;
+    if (Time < 12 == Time > 0) {
+        Response = "It's morning.";
+    }
+    else if (Time >12 == Time < 18) {
+        Response = "It's afternoon."  ;   
+    } 
+    else {
+        Response = "It's Evening.";
+    }
+    document.getElementById("Hello").innerHTML = Response;
+}
+// END OF FOURTH ASSIGNMENT
