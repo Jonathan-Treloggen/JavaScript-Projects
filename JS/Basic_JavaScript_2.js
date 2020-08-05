e930a0653b48090fcf5df5e7a6c9b799e013b47b
Jonathan Treloggen <53909409+Jonathan-Treloggen@users.noreply.github.com>
2020-08-04 17:47:26 -0700

    Delete Basic_JavaScript_2.js

--- JS/Basic_JavaScript_2.js
+++
@@ -1,9 +0,0 @@
-function displayDate() {
-    document.getElementById("when").innerHTML= Date();
-}
-
-function Instructions() {
-    var L = "Well, above this button is another button, click it.";
-    L += "If you don\'t like the answer it gave you, refresh the page and click it again.";
-    document.getElementById("hmmm").innerHTML = L;
-}
